3494420487c1db0d57b6083888b6901959b20a5d
Shan Allen Bacsain <48052318+sabacsain@users.noreply.github.com>
2023-11-13 21:43:31 +0800

    Update - new sender email function

--- bot.py
+++ bot.py
@@ -8,22 +8,32 @@
 
 # Imports
 # =============================================================================
-import smtplib
-import random
-import os
+import smtplib, random, os, json
+
 
 # =============================================================================
 # SET EMAIL LOGIN REQUIREMENTS
 # =============================================================================
-gmail_user = 'killuadummy2000@gmail.com'
-gmail_app_password = 'afmgchkeaeadtvjt'
+
+# GETTING THE PYTHON SCRIPT PATH
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+# READ SENDER EMAIL LIST
+with open(current_path + '/list_sender.txt') as f: 
+    sender_list = f.read() 
+
+# CONVERT STR INTO DICT
+sender_dict = json.loads(sender_list) 
+
+# RANDOMIZE GMAIL SENDER TO BE USED 
+random_index = random.randint(0, len(sender_dict) - 1)
+gmail_user = list(sender_dict.keys())[random_index]
+gmail_app_password = sender_dict[gmail_user]
+
 
 # =============================================================================
 # SET THE INFO ABOUT THE SAID EMAIL
 # =============================================================================
-
-# GETTING THE PYTHON SCRIPT PATH
-current_path = os.path.dirname(os.path.abspath(__file__))
 
 # CONVERTS NAME FILE TXT INTO LIST
 with open(current_path + '/list_names.txt', 'r') as file:
@@ -37,30 +47,31 @@ with open(current_path + '/list_subjects.txt', 'r') as file:
 #with open(current_path + '/list_body.txt', 'r') as file:
 #    body_list = [line.split(',') for line in file]
 
-body_list = [f"""Hey {random.choice(name_list)},
+body_list = [
+    f"""Hey {random.choice(name_list)},
                 
-Hope this email finds you well! It's been a while since we caught up, and I was thinking it's high time we grab a coffee and chat about life. How about we meet up this week? I found this cozy new coffee spot downtown that I've been dying to try.
+# Hope this email finds you well! It's been a while since we caught up, and I was thinking it's high time we grab a coffee and chat about life. How about we meet up this week? I found this cozy new coffee spot downtown that I've been dying to try.
 
-Let me know what day works for you, and we can make it happen. Looking forward to some good laughs and catching up!
+# Let me know what day works for you, and we can make it happen. Looking forward to some good laughs and catching up!
                 
-Cheers,
-{random.choice(name_list)}""",
+# Cheers,
+# {random.choice(name_list)}""",
 
-             f"""Hey, what's up? friend!
+    f"""Hey, what's up? friend!
                 
 I hope you have been well!
 
 Cheers,
 {random.choice(name_list)}""",
 
-            f"""Hi {random.choice(name_list)},
+    f"""Hi {random.choice(name_list)},
                 
 Long time, no talk! How about a quick coffee catch-up or a virtual chat? I've got some updates to share and would love to hear what's new with you. Let me know when you're free!
 
 Yours,
 {random.choice(name_list)}""",
 
-            f"""Hi {random.choice(name_list)},
+    f"""Hi {random.choice(name_list)},
 
 How are you doing? I haven't talked to you in a while, so I wanted to reach out and see how you're doing.
 
@@ -68,14 +79,14 @@ I've been keeping busy with my acads. I'm also really excited about seeing you s
 
 I hope to hear from you soon!""",
 
-            f"""Hey {random.choice(name_list)},
+    f"""Hey {random.choice(name_list)},
 
 Got some good news to share. Let's celebrate over dinner tonight!
 
 Best,
 {random.choice(name_list)}""",
 
-            f"""Hey {random.choice(name_list)},
+    f"""Hey {random.choice(name_list)},
 
 I just saw you and thought of you.
 
@@ -84,7 +95,7 @@ Let me know what you've been up to lately. I'd love to catch up sometime soon.
 Talk to you soon,
 {random.choice(name_list)}""",
 
-            f"""Hey {random.choice(name_list)},
+    f"""Hey {random.choice(name_list)},
 
 Happy Holidays!
 
@@ -95,7 +106,7 @@ I'm sending you my best wishes.
 Talk to you soon,
 {random.choice(name_list)}""",
 
-            f"""Hi {random.choice(name_list)},
+    f"""Hi {random.choice(name_list)},
 
 I just wanted to say that I'm thinking of you today.
 
