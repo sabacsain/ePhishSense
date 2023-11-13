eed0e4aa397ac93cf3b42676aaa39200ecd2ac10
Shan Allen Bacsain <48052318+sabacsain@users.noreply.github.com>
2023-11-13 20:24:24 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,19 +35,28 @@ with open(current_path + '/list_subjects.txt', 'r') as file:
 #with open(current_path + '/list_body.txt', 'r') as file:
 #    body_list = [line.split(',') for line in file]
 
-body_list = [f"""Hey Peter,\n\n
-                Hope this email finds you well! It's been a while since we caught up, and I was thinking it's high time we grab a coffee and chat about life. How about we meet up this week? I found this cozy new coffee spot downtown that I've been dying to try.\n\n
-                 Let me know what day works for you, and we can make it happen. Looking forward to some good laughs and catching up!\n\n
-                 Cheers,\n
-                 {random.choice(name_list)}""",
-             f"""Hey, what's up? friend!\n\n
-                 I hope you have been well!\n\n
-                 Cheers,\n
-                 {random.choice(name_list)}"""],
-             f"""Hi {random.choice(name_list)},\n\n
-Long time, no talk! How about a quick coffee catch-up or a virtual chat? I've got some updates to share and would love to hear what's new with you. Let me know when you're free!\n\n
-Yours,\n
-{random.choice(name_list)}"""
+body_list = [f"""Hey {random.choice(name_list)},
+                
+Hope this email finds you well! It's been a while since we caught up, and I was thinking it's high time we grab a coffee and chat about life. How about we meet up this week? I found this cozy new coffee spot downtown that I've been dying to try.
+
+Let me know what day works for you, and we can make it happen. Looking forward to some good laughs and catching up!
+                
+Cheers,
+{random.choice(name_list)}""",
+
+             f"""Hey, what's up? friend!
+                
+I hope you have been well!
+
+Cheers,\n
+{random.choice(name_list)}""",
+
+            f"""Hi {random.choice(name_list)},
+                
+Long time, no talk! How about a quick coffee catch-up or a virtual chat? I've got some updates to share and would love to hear what's new with you. Let me know when you're free!
+
+Yours,
+{random.choice(name_list)}"""]
 
 sent_from = gmail_user
 sent_to = ['ephishsense@gmail.com', 'ephishsense@gmail.com']  	    # palitan na lang 'to if san gusto i-send
